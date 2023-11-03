1628008fa4da059f27c05fd9de4d6ba9abeb4456
Changhoon-Jee <chjee71@gmail.com>
2023-11-03 13:29:42 +0900

    Added & applied app configs.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'verbose'],
+    cors: { origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' },
+  });
+
+  app.enableShutdownHooks();
+
+  const configService = app.get(ConfigService);
+  const listenPort = configService.get('PORT', 3000);
+  await app.listen(listenPort);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
