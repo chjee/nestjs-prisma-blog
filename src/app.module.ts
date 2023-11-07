8cad9e4205abcf451ff0ba8524e7cf7e1b224c9b
Changhoon-Jee <chjee71@gmail.com>
2023-11-07 13:15:35 +0900

    remove app controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
@@ -17,7 +16,6 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
     PostModule,
     AuthModule,
   ],
-  controllers: [AppController],
   providers: [{ provide: 'APP_GUARD', useClass: JwtAuthGuard }],
 })
 export class AppModule {}
