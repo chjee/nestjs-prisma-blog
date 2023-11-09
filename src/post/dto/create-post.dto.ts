caac3bfcf99837a51f79605f35cb7a6ff43beaae
Changhoon-Jee <chjee71@gmail.com>
2023-11-09 20:05:08 +0900

    add & apply api tags

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNotEmpty,
@@ -8,14 +9,29 @@ import {
 } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'Post Title',
+    example: 'Just 10 minutes.',
+    minLength: 2,
+    maxLength: 60,
+  })
   @IsNotEmpty()
   @IsString()
   @Length(2, 60)
   readonly title: string;
 
+  @ApiProperty({
+    description: 'published or not',
+    example: false,
+  })
   @IsBoolean()
   readonly published: boolean;
 
+  @ApiProperty({
+    description: 'Post User Id',
+    example: 1,
+    minimum: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,22 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { $Enums } from '@prisma/client';
 import { IsEmail, IsEnum, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User Name',
+    example: 'Andrew',
+    minLength: 4,
+    maxLength: 60,
+  })
   @IsNotEmpty()
   @IsString()
   @Length(4, 60)
   readonly username: string;
 
+  @ApiProperty({
+    description: 'User Password',
+    example: 'whoami',
+    minLength: 6,
+    maxLength: 60,
+  })
   @IsNotEmpty()
   @IsString()
   @Length(6, 60)
   readonly password: string;
 
+  @ApiProperty({
+    description: 'User Email',
+    example: 'andrew@prisma.io',
+    minLength: 6,
+    maxLength: 60,
+  })
   @IsNotEmpty()
   @IsEmail()
   @Length(6, 60)
   readonly email: string;
 
+  @ApiProperty({
+    description: 'User Role',
+    example: 'USER',
+    enum: ['ADMIN', 'USER'],
+  })
   @IsString()
   @IsEnum(['ADMIN', 'USER'])
   readonly role: $Enums.Role;
