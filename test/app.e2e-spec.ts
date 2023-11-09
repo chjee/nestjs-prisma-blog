596913554efbe2dd3e8e6364cd6253391246c851
Changhoon-Jee <chjee71@gmail.com>
2023-11-09 17:25:05 +0900

    add & supplement user part.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,103 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { JwtAuthGuard } from './../src/auth/jwt-auth.guard';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  const mockUser = {
+    id: 1,
+    username: 'Alice',
+    password: 'whoami',
+    email: 'alice@prisma.io',
+    role: 'USER',
+  };
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(JwtAuthGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    app.enableShutdownHooks();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/auth/login', () => {
+    it('POST 200', () => {
+      return request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ username: mockUser.username, password: mockUser.password })
+        .expect(HttpStatus.OK);
+    });
+  });
+
+  describe('/user', () => {
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/user')
+        .send({
+          email: 'andrew@prisma.io',
+          username: 'Andrew',
+          password: 'whoami',
+          role: 'ADMIN',
+        })
+        .expect(HttpStatus.CREATED)
+        .expect((res) => {
+          mockUser.id = res.body.id;
+          console.log(mockUser);
+        });
+    });
+
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/user')
+        .query({ skip: 0, take: 3 })
+        .expect(HttpStatus.OK);
+    });
+
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get(`/user/${mockUser.id}`)
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          console.log(res.body);
+        });
+    });
+
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch(`/user/${mockUser.id}`)
+        .send({ username: 'andrew', role: 'USER' })
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          console.log(res.body);
+        });
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete(`/user/${mockUser.id}`)
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          console.log(res.body);
+        });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
