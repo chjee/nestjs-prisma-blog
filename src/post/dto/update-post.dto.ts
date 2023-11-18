9fb46ad113816b0f805b700326d85852359db535
Changhoon-Jee <chjee71@gmail.com>
2023-11-18 20:06:29 +0900

    change class type to PickType.

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PickType(CreatePostDto, [
+  'title',
+  'published',
+] as const) {}
