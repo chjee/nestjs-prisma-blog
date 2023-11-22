e64872179c65a02e626ead215deb7b419e4c85cc
Changhoon-Jee <chjee71@gmail.com>
2023-11-22 15:08:18 +0900

    adjust global module setting.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
     AuthModule,
     UserModule,
     PostModule,
+    PrismaModule,
   ],
   providers: [
     { provide: 'APP_GUARD', useExisting: JwtAuthGuard },
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [PostController],
   providers: [PostService],
 })
