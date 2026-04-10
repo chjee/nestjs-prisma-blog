185ede95a9b0981ad17333887191600d728128e9
Changhoon Jee <chjee71@gmail.com>
2026-04-10 15:48:38 +0900

    chore: add docker-compose, Joi env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './health/health.module';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { PostModule } from './post/post.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+
+const Joi = require('joi');
 
 @Module({
   imports: [
@@ -14,6 +16,14 @@ import { ThrottlerModule } from '@nestjs/throttler';
       isGlobal: true,
       cache: true,
       envFilePath: ['.env.dev', '.env'],
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+        DATABASE_URL: Joi.string().uri({
+          scheme: ['mysql', 'mysql2'],
+        }).required(),
+        JWT_SECRET: Joi.string().min(1).required(),
+        ALLOWED_ORIGINS: Joi.string().min(1).required(),
+      }),
     }),
     ThrottlerModule.forRoot([
       {
